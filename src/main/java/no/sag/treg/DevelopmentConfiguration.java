d20d4805e3b6ce466f3cf3b5f7071d5bdb7c9cc6
Steinar Glåmeter <sglamseter@gmail.com>
2016-07-15 15:35:48 +0200

    cleanup

--- src/main/java/no/sag/treg/DevelopmentConfiguration.java
+++ src/main/java/no/sag/treg/DevelopmentConfiguration.java
@@ -3,7 +3,6 @@ package no.sag.treg;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,34 +15,16 @@ public class DevelopmentConfiguration
 {
     private static final Logger LOG = LoggerFactory.getLogger(ProductionConfiguration.class);
 
-   /* @Bean
-    public DataSource dataSource()
-    {
-        LOG.info("Using development datasource...");
-
-        return DataSourceBuilder.create()
-            .url("jdbc:mariadb://localhost:3306/innebandy")
-            .driverClassName("org.mariadb.jdbc.Driver")
-            .username("root")
-            .password("Vinter2015")
-            .build();
-    }*/
-
     @Bean
     public DataSource dataSource()
     {
         LOG.info("Using development datasource...");
 
         final HikariDataSource ds = new HikariDataSource();
-       // ds.setMaximumPoolSize(30);
         ds.setDataSourceClassName("org.mariadb.jdbc.MariaDbDataSource");
         ds.addDataSourceProperty("url", "jdbc:mariadb://localhost:3306/innebandy");
-        ds.addDataSourceProperty("user", "glamseter");
-        ds.addDataSourceProperty("password", "qTDYPrLBGk66weq");
-      /*  ds.addDataSourceProperty("cachePrepStmts", true);
-        ds.addDataSourceProperty("prepStmtCacheSize", 250);
-        ds.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
-        ds.addDataSourceProperty("useServerPrepStmts", true);*/
+        ds.addDataSourceProperty("user", "root");
+        ds.addDataSourceProperty("password", "Vinter2015");
         return ds;
     }
 }
--- src/main/java/no/sag/treg/ProductionConfiguration.java
+++ src/main/java/no/sag/treg/ProductionConfiguration.java
@@ -3,7 +3,6 @@ package no.sag.treg;
 import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,37 +15,16 @@ public class ProductionConfiguration
 {
     private static final Logger LOG = LoggerFactory.getLogger(ProductionConfiguration.class);
 
-  /*  @Bean
-    public DataSource dataSource()
-    {
-        LOG.info("Using production datasource...");
-
-            return DataSourceBuilder.create()
-            .url("jdbc:mariadb://glamseter.mysql.domeneshop.no:3306/glamseter")
-            .driverClassName("org.mariadb.jdbc.Driver")
-            .username("glamseter")
-            .password("qTDYPrLBGk66weq")
-            .build();
-    }*/
-
     @Bean
     public DataSource dataSource()
     {
         LOG.info("Using production datasource...");
 
         final HikariDataSource ds = new HikariDataSource();
-       // ds.setMaximumPoolSize(30);
         ds.setDataSourceClassName("org.mariadb.jdbc.MariaDbDataSource");
         ds.addDataSourceProperty("url", "jdbc:mariadb://glamseter.mysql.domeneshop.no:3306/glamseter");
         ds.addDataSourceProperty("user", "glamseter");
         ds.addDataSourceProperty("password", "qTDYPrLBGk66weq");
-      /*  ds.addDataSourceProperty("cachePrepStmts", true);
-        ds.addDataSourceProperty("prepStmtCacheSize", 250);
-        ds.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
-        ds.addDataSourceProperty("useServerPrepStmts", true);*/
         return ds;
     }
-
-
-
 }
